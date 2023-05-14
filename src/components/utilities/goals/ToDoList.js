95eb7ad4cdcfd12cfedd00d652401fb038e85471
Vinh T Nguyen <imvinhng@github.com>
2023-05-14 09:50:25 -0700

    Fixed bugs in Goals

--- src/components/utilities/goals/ToDoList.js
+++ src/components/utilities/goals/ToDoList.js
@@ -3,14 +3,15 @@ import ToDo from './ToDo';
 
 const ToDoList = ({toDoList, handleToggle, handleFilter}) => {
     return (
-        <div className="todo-div">
+        <div>
             {toDoList.map(todo => {
                 return (
                     <ToDo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter}/>
                 )
             })}
-            <button style={{margin: '20px'}} className="todo-btn" onClick={handleFilter}>CLEAR COMPLETED</button>
-
+            <div className="todo-div">
+                <button style={{margin: '20px'}} className="todo-btn" onClick={handleFilter}>CLEAR COMPLETED</button>
+            </div>
         </div>
     );
 };
